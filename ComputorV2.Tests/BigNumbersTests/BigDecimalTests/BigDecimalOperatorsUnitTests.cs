c84644b35f1d7333d3d689de05194747782a2eac
Nataliia <n.kolosova@levi9.com>
2020-02-20 16:33:38 +0200

    Test revolution p1

---
+++ ComputorV2.Tests/BigNumbersTests/BigDecimalTests/BigDecimalOperatorsUnitTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using BigNumbers;
+using NUnit.Framework;
+
+namespace ComputorV2.UnitTests.BigNumbersTests.BigDecimalTests
+{
+    [TestFixture]
+    public class BigDecimalOperatorsUnitTests
+    {
+
+        [Test]
+        [TestCase("0", "0", "0")]
+        [TestCase("0", "-5", "-5")]
+        [TestCase("-5", "0", "-5")]
+        [TestCase("-5", "6", "1")]
+        [TestCase("123.45", "45.678", "169.128")]
+        [TestCase("-3.1", "10.005", "6.905")]
+        [TestCase("-10.005", "-3.1", "-13.105")]
+        public void AddOperator_WhenCalled_ShouldReturnSumOfTwoNumbers(string op1, string op2, string expected)
+        {
+            BigDecimalTestHelper.DoTesting(op1, op2, expected, Operation.Add);
+        }
+
+        [Test]
+        [TestCase("0", "0", "0")]
+        [TestCase("0", "-5", "5")]
+        [TestCase("-5", "0", "-5")]
+        [TestCase("-5", "6", "-11")]
+        [TestCase("123.45", "45.678", "77.772")]
+        [TestCase("-3.1", "10.005", "-13.105")]
+        [TestCase("-10.005", "-3.1", "-6.905")]
+        public void SubOperator_WhenCalled_ShouldReturnSubOfTwoNumbers(string op1, string op2, string expected)
+        {
+            BigDecimalTestHelper.DoTesting(op1, op2, expected, Operation.Sub);
+        }
+
+        [Test]
+        [TestCase("0", "0", "0")]
+        [TestCase("0", "-5", "0")]
+        [TestCase("-5", "0", "0")]
+        [TestCase("-5", "6", "-30")]
+        [TestCase("123.45", "45.678", "5638.9491")]
+        [TestCase("-3.1", "10.005", "-31.0155")]
+        [TestCase("-10.005", "-3.1", "31.0155")]
+        public void MulOperator_WhenCalled_ShouldReturnMulOfTwoNumbers(string op1, string op2, string expected)
+        {
+            BigDecimalTestHelper.DoTesting(op1, op2, expected, Operation.Mul);
+        }
+
+        [Test]
+        [TestCase(Operation.Add)]
+        [TestCase(Operation.Sub)]
+        [TestCase(Operation.Mul)]
+        [TestCase(Operation.Div)]
+        [TestCase(Operation.Mod)]
+        public void Operators_10000RandomTests(Operation operation)
+        {
+            for (int i = 0; i < 10000; i++)
+                BigDecimalTestHelper.ExecuteRandomTest(operation);
+        }
+    }
+}
---
+++ ComputorV2.Tests/BigNumbersTests/BigDecimalTests/BigDecimalTestHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BigNumbers;
+using NUnit.Framework;
+
+namespace ComputorV2.UnitTests.BigNumbersTests.BigDecimalTests
+{
+    public enum Operation
+    {
+        Add, Sub, Mul, Div, Mod
+    }
+
+    static class BigDecimalTestHelper
+    {
+        private static readonly Random rand = new Random((int) DateTime.Now.Ticks);
+        private static int _halfMaxInt = int.MaxValue / 2;
+
+        public static void DoTesting(string left, string right, string expectedResult, Operation operation)
+        {
+            var a = new BigDecimal(left);
+            var b = new BigDecimal(right);
+            BigDecimal c = _bnFunctions[operation](a, b);
+            try
+            {
+                Assert.AreEqual(expectedResult, c.ToString());
+            }
+            catch (AssertionException)
+            {
+                throw new Exception(
+                    $"A = {a}, B = {b}.\n Expected result is {expectedResult}\n Actual result is{c}");
+            }
+        }
+
+        public static string DecimalToString(decimal number)
+        {
+            string str = number.ToString();
+            if (!str.Contains(",") && !str.Contains("."))
+                return str;
+            str = str.Replace(",", ".");
+            StringBuilder sb = new StringBuilder(str);
+            while (sb[sb.Length - 1] == '0')
+                sb.Remove(sb.Length - 1, 1);
+            if (sb[sb.Length - 1] == '.')
+                sb.Remove(sb.Length - 1, 1);
+            return sb.ToString();
+        }
+
+        public static void ExecuteRandomTest(Operation operation)
+        {
+            decimal a = GenerateRandomInt() / 10000;
+            decimal b = GenerateRandomInt() / 100000;
+            decimal c = _decFunctions[operation](a, b);
+
+            DoTesting(DecimalToString(a),
+                DecimalToString(b),
+                DecimalToString(c),
+                operation);
+        }
+
+        private static int GenerateRandomInt()
+        {
+            return rand.Next(0, int.MaxValue) - _halfMaxInt;
+        }
+
+        #region Operations Delegates
+
+        private static readonly Func<BigDecimal, BigDecimal, BigDecimal> _addBnFunction = (x, y) => x + y;
+        private static readonly Func<decimal, decimal, decimal> _addDecFunction = (x, y) => x + y;
+
+        private static readonly Func<BigDecimal, BigDecimal, BigDecimal> _subBnFunction = (x, y) => x - y;
+        private static readonly Func<decimal, decimal, decimal> _subDecFunction = (x, y) => x - y;
+
+        private static readonly Func<BigDecimal, BigDecimal, BigDecimal> _mulBnFunction = (x, y) => x * y;
+        private static readonly Func<decimal, decimal, decimal> _mulDecFunction = (x, y) => x * y;
+
+        private static readonly Func<BigDecimal, BigDecimal, BigDecimal> _divBnFunction = (x, y) => x / y;
+        private static readonly Func<decimal, decimal, decimal> _divDecFunction = (x, y) => x / y;
+
+        private static readonly Func<BigDecimal, BigDecimal, BigDecimal> _modBnFunction = (x, y) => x % y;
+        private static readonly Func<decimal, decimal, decimal> _modDecFunction = (x, y) => x % y;
+
+        private static Dictionary<Operation, Func<BigDecimal, BigDecimal, BigDecimal>> _bnFunctions =
+            new Dictionary<Operation, Func<BigDecimal, BigDecimal, BigDecimal>>
+        {
+            {Operation.Add, _addBnFunction },
+            {Operation.Sub, _subBnFunction },
+            {Operation.Mul, _mulBnFunction },
+            {Operation.Div, _divBnFunction },
+            {Operation.Mod, _modBnFunction }
+        };
+
+        private static Dictionary<Operation, Func<decimal, decimal, decimal>> _decFunctions =
+            new Dictionary<Operation, Func<decimal, decimal, decimal>>
+        {
+            {Operation.Add, _addDecFunction },
+            {Operation.Sub, _subDecFunction },
+            {Operation.Mul, _mulDecFunction },
+            {Operation.Div, _divDecFunction },
+            {Operation.Mod, _modDecFunction }
+        };
+
+        #endregion Operations Delegates
+
+    }
+}
