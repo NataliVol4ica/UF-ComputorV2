d9679cf237c971fa8391cac5cb863658b7cc079b
Nataliia <n.kolosova@levi9.com>
2020-02-24 17:11:13 +0200

    updated tests a little; remember to check them simultaneously!

--- ComputorV2.IntegrationTests/ComputorIntegrationTests.cs
+++ ComputorV2.IntegrationTests/ComputorIntegrationTests.cs
@@ -20,8 +20,27 @@ namespace ComputorV2.IntegrationTests
         }
 
         [Test]
+        public void StartReading_EvaluatingFunction_ComplexTest()
+        {
+            //Arrange
+            _consoleProcessor
+                .SetupSequence(cp => cp.ReadLine())
+                .Returns("f(c) = c + c * c")
+                .Returns("b=9")
+                .Returns("a = f(0) + f(1) +   f(b ^ 3 + 1) + f(f(2))")
+                .Returns("Exit");
+            var testedComputor = new Computor(_consoleProcessor.Object);
+
+            //Act
+            testedComputor.StartReading();
+
+            //Assert
+            _consoleProcessor.Verify(cp => cp.WriteLine("> 533674"));
+        }
+
+        [Test]
         [TestCase("func(x)=2", "func(X) = 2")]
-        [TestCase("func(BB)=BB^2+2BB+1", "func(X) = X ^ 2 + 2 X + 1")]
+        [TestCase("func(BB)=BB^2+2*BB+1", "func(X) = X ^ 2 + 2 * X + 1")]
         public void StartReading_WhenDeclaringFunc_PrintsResult(string cmd, string expectedOutput)
         {
             _consoleProcessor
@@ -70,34 +89,37 @@ namespace ComputorV2.IntegrationTests
         }
 
         [Test]
-        public void StartReading_EvaluatingFunction_ComplexTest()
+        public void StartReading_WhenSolvingSimplestExpression_ShouldPrintsSolution()
         {
             _consoleProcessor
                 .SetupSequence(cp => cp.ReadLine())
-                .Returns("f(c) = c + c * c")
-                .Returns("b=9")
-                .Returns("a = f(0) + f(1) +   f(b ^ 3 + 1) + f(f(2))")
+                .Returns("x = 7 + 2")
+                .Returns("x= ?")
                 .Returns("Exit");
 
             var testedComputor = new Computor(_consoleProcessor.Object);
             testedComputor.StartReading();
 
-            _consoleProcessor.Verify(cp => cp.WriteLine("> 533674"));
+            _consoleProcessor.Verify(cp => cp.WriteLine("> 9"));
         }
 
         [Test]
-        public void StartReading_SolvingSimplestEquation_PrintsSolution(string func, string solution)
+        public void StartReading_WhenSolvingSimplestEquation_ShouldPrintsSolution()
         {
             _consoleProcessor
                 .SetupSequence(cp => cp.ReadLine())
-                .Returns("x = 0 ?")
+                .Returns("f(x) = 0")
+                .Returns("F(x)= ?")
                 .Returns("Exit");
 
             var testedComputor = new Computor(_consoleProcessor.Object);
             testedComputor.StartReading();
 
-            _consoleProcessor.Verify(cp => cp.WriteLine($"> {solution}"));
+            _consoleProcessor.Verify(cp => cp.Write(
+                It.Is<string>(s => s.Contains($"X = 0"))));
         }
+
+        #region Solve x^2 Equation
 
         [Test]
         [TestCase("x", "0")]
@@ -112,7 +134,11 @@ namespace ComputorV2.IntegrationTests
             var testedComputor = new Computor(_consoleProcessor.Object);
             testedComputor.StartReading();
 
-            _consoleProcessor.Verify(cp => cp.WriteLine($"> {solution}"));
+            _consoleProcessor.Verify(cp => cp.Write(
+                It.Is<string>(s=>s.Contains($"X = {solution}"))));
         }
+
+        #endregion Solve Equation
+
     }
 }
